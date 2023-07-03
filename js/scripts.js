15e9fbf3216bffca81f984adca0723f9449782cf
alessiovietri <info@alessiovietri.it>
2023-07-03 12:06:10 +0200

    Completato LC

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,34 @@
-console.log('Template HTML CSS JS');
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: 'Hello Vue!',
+            names: ['Michele', 'Fabio', 'Roberto'],
+            newName: ''
+        }
+    },
+    methods: {
+        addToNames() {
+            this.names.push(this.newName);
+
+            this.newName = '';
+        }
+    }
+}).mount('#app');
+
+// for (let key in obj) {
+//     console.log(obj[key]);
+// }
+
+// for (let i in arr) {
+//     console.log(arr[i]);
+// }
+
+// names.forEach(element => {
+    
+// });
+
+// names.forEach((element, index) => {
+    
+// });
